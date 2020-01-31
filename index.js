09168b8671d0d62b07019acbcdcf3c003a6d69cc
Brian Henry <brian.henry@delivered.io>
2020-01-31 17:04:44 -0600

    add stuff

--- index.js
+++ index.js
@@ -1,33 +1,71 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
+
+const extractTrelloCardId(prBody) {
+  console.log(`pr body: ${prBody}`);  
+  
+  //find 1st instance of trello card url - must be 1st thing in PR
+  const matches = /^\s*https\:\/\/trello\.com\/c\/(\w+)/.exec(prBody);
+  const cardId = matches && matches[1];
+  console.log(`card id = ${cardId}`);
+
+  return cardId;
+}
+
+const getCardAttachments = async (cardId) => {
+  return instance.get(`/1/cards/${cardId}/attachments`, {
+      data: {}, 
+      params: {
+          key: trelloKey, 
+          token: trelloToken
+      }
+  }).then((res) => {
+    console.log(`status: ${res.status}.  data follows:`);
+    console.dir(res.data)
+    return res.data;
+  })
+};
+
 
 try {
   //TOOD chcek if failure will stop the PR from being mergeable?  well it does mark the build failed.  if we have checks on that it could be a problem.
 
-  const reggie = /^\s*https\:\/\/trello\.com\/c\/(\w+)/;
-  
-  console.log(`pr body: ${github.context.payload.pull_request.body}`);
-  const body = github.context.payload.pull_request.body;
-
   const trelloKey = core.getInput('trello-key');
   const trelloToken = core.getInput('trello-token');
 
-  console.log(`TRELLO_KEY from workflow repo: ${trelloKey}`);
+  const cardId = extractTrelloCardId(github.context.payload.pull_request.body);
   
   //TOOD check if attachment already present?  if we allow this to run on edit, or even if just run and somebody already added on trello side, be graceful.
   // yeah, check https://api.trello.com/1/cards/CIqx54dG/attachments and if not empty it'll be an array of obj with 'name' of a github url
   
-  //find 1st instance of trello card url - must be 1st thing in PR
-  const matches = reggie.exec(body);
-  console.log(`card id = ${matches && matches[1]}`);
+  if(cardId) {
+    //hmmmmmm.  will the container know to wait for this async - docs don't mention this?
+    (async () => {
+      try {
+        const attachments = await getCardAttachments(cardId);
+      } catch(err) {
+        console.log(`error fetching attachments: ${err}`);
+        if(err.response) {
+          //console.log(`status: ${err.status}.  error data follows:`);
+          console.dir(err.response.data);
+        }
+        throw err;
+      }
+
+
+
 
   
+    })();
+  }
+
   
-  console.log('');
-  
-  
-  console.dir(github.context);
-  console.log('');
+  //console.log('');
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+
+  //console.log('');
   
   // TODO add badge if it works?
   
@@ -35,13 +73,9 @@ try {
   
   
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+
+  //core.setOutput("time", time);
+
 } catch (error) {
   core.setFailed(error.message);
 }
