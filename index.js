df6fc874328e0c21cf854c9f3ef8726a1d0c19e4
Brian Henry <brian.henry@delivered.io>
2020-01-31 14:50:51 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+  //TOOD chcek if failure will stop the PR from being mergeable?
+  
+  
+  console.log(`pr body: ${github.context.payload.pull_request.body}`);
+  const body = github.context.payload.pull_request.body;
+
+  //find 1st instance of trello card url - must be 1st thing in PR
+  console.log(`card id = ${/^\s*https\:\/\/trello\.com\/c\/([^\/#?]+)/.exec(body)[1]}`);
+
+  
+  console.log('');
+  
+  
   console.dir(github.context);
   console.log('');
   
-  console.log(`pr body: ${github.context.payload.pull_request.body}`);
-  
-  console.log('');
-  
+  // TODO add badge if it works?
   
   
   // `who-to-greet` input defined in action metadata file
